4120ef01249b09ded06bbaa556adaaeab4aa0771
Refinaldii <refinaldi9786@gmail.com>
2024-10-22 11:07:54 +0700

    first commit

---
+++ lamba.py
@@ -0,0 +1,11 @@
+
+
+
+import math
+
+luas_lingkaran = lambda r: math.pi * r*r
+
+
+jari_jari = 7
+area = luas_lingkaran(jari_jari)
+print (f"Luas Lingkaran dengan Jari Jari {jari_jari} adalah {area:.2f}")
