fab514bd0b29931abce7be3a8fc9b11abfad9d87
Refinaldii <refinaldi9786@gmail.com>
2024-10-22 11:10:52 +0700

    Update komen

--- lamba.py
+++ lamba.py
@@ -1,11 +1,17 @@
-
-
-
-import math
-
+#digunakan untuk mengimpor modul math, yang menyediakan berbagai fungsi matematika 
+# seperti trigonometri, logaritma, dan fungsi eksponensial, serta konstanta seperti pi.
+import math 
 luas_lingkaran = lambda r: math.pi * r*r
 
+#jari jari untuk lingkaran = 7
+jari_jari = 7 
 
-jari_jari = 7
-area = luas_lingkaran(jari_jari)
+# Memanggil fungsi luas_lingkaran dengan argumen jari_jari, 
+# lalu hasil perhitungan (luas lingkaran) disimpan dalam variabel area.
+area = luas_lingkaran(jari_jari) 
+
+
+#perintah untuk mencetak teks dengan interpolasi variabel jari_jari dan area, 
+# di mana nilai area dibulatkan hingga dua tempat desimal menggunakan format string f dan format spesifik {:.2f}
 print (f"Luas Lingkaran dengan Jari Jari {jari_jari} adalah {area:.2f}")
+
